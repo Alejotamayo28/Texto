9db13997909aba06795d655463bbd6620547f149
Alejandro Vergara Tamayo <73759053+Alejotamayo28@users.noreply.github.com>
2023-03-07 12:30:02 -0500

    Add files via upload

---
+++ Encontrar.js
@@ -0,0 +1,5 @@
+
+
+let texto =
+  "Tito no es un mono cualquiera. A Tito no le gusta trepar por los arboles  y odia comer platanos. El prefiere pasear por el bosque, oler las flores y recoger las nueces que se caen de los arboles";
+
